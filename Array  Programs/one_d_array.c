b0f786cd51870fdef72adfc09532abc4848a4aa8
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2023-10-19 22:31:03 +0530

    Added deletion from array. Iter 1

--- Array  Programs/one_d_array.c
+++ Array  Programs/one_d_array.c
@@ -2,13 +2,37 @@
 #include <stdlib.h>
 
 // All about 1D arrays
-void display(int a[], int size){
+
+void display(int array[], int size){
     for(int i = 0; i<size; i++){
-        printf("%d\t", a[i]);
+        printf("%d\t", array[i]);
     }
+    printf("\n");
 }
 
+// adding element to array
+void append(int array[], int size){
+    int position, value, temp;
+    printf("Enter element you want to enter: ");
+    scanf("%d", &value);
+    printf("Enter position you want to enter element at: ");
+    scanf("%d", &position);
+    for(int i=size-1;i>=position-1;i--)
+      array[i+1]=array[i];
+    array[position-1]= value;
+}
+
+// Deletion of elements from array
+void deletion(int array[], int size){
+    int pos;
+    printf("Enter index of element to delete: ");
+    scanf("%d", &pos);
+    for(int i = pos; i<size;  i++){
+        array[i-1] = array[i];
+    }
+}
 int main(){
+    // Declaring an array that is of dynamic size using malloc()
     int *arr = (int*) malloc(100 * sizeof(int));
     int n;
 
@@ -19,5 +43,27 @@ int main(){
         scanf("%d", &arr[i]);
     }
 
-    display(arr,n);
+    int choice, out=0;
+    while (out == 0){
+        printf("What operation to perform on array?\n1) Display Array\n2) Add element to array\n3) Delete element from array\n4) Exit\nEnter Choice: ");
+        scanf("%d", &choice);
+        switch (choice)
+        {
+        case 1:
+            display(arr, n);
+            break;
+        case 2:
+            append(arr, n);
+            break;
+        case 3:
+            deletion(arr, n);
+            break;
+        case 4:
+            out = 1;
+            break;
+        default:
+            printf("Invalid Choice!\n");
+            break;
+        }
+    }
 }
