102648d4682084dcbb72a8b7d7a3083760ab9edd
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2023-10-30 13:42:26 +0530

    modify\two_d_array.c

--- Array  Programs/two_d_array.c
+++ Array  Programs/two_d_array.c
@@ -1,24 +1,38 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void display(int *arr, int r, int c){
-    for (int i=0; i<c*r; i++){
-        printf("%d\t", arr[i]);
-    }
-}
-
 int main(){
-    int r,c;
-    printf("Enter the number of rows you want: ");
-    scanf("%d", &r);
-    printf("\nEnter the number of columns you want: ");
-    scanf("%d", &c);
-    int *ptr = malloc(r*c * sizeof(int));
-    for(int i =0; i<r*c; i++){
-        scanf("%d",ptr[i]); 
+    int a[3][3], b[3][3], mul[3][3];
+
+    printf("Enter array one: \n");
+    for(int i =0; i<3; i++){
+        for(int j= 0; j<3; j++){
+            scanf("%d", &a[i][j]);
+        }
     }
-    printf("\n");
-    display(ptr, r,c);
-    free(ptr);
+
+    printf("\nEnter array two: \n");
+    for(int i =0; i<3; i++){
+        for(int j= 0; j<3; j++){
+            scanf("%d", &b[i][j]);
+        }
+    }
+
+    // Multiply
+    for(int i=0;i<3;i++){    
+        for(int j=0;j<3;j++){    
+            mul[i][j]=0;    
+            for(int k=0;k<3;k++){    
+                mul[i][j]+=a[i][k]*b[k][j];    
+            } 
+       }    
+    }
+
+    for(int i =0; i<3; i++){
+        for(int j= 0; j<3; j++){
+            printf("%d\t", mul[i][j]);
+        }
+        printf("\n");
+    }
     return 0;
 }
