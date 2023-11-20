142d4480fd1b445ee56fd30d0d951cac66840d9d
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2023-11-20 22:29:08 +0530

    add/circle.c

---
+++ Math Programs/Areas and Volumes/circle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+// To calculate the area and perimeter of a circle
+
+#define PI 3.1415
+
+int main(){
+    float radius;
+    printf("Enter radius of circle: ");
+    scanf("%f", &radius);
+    printf("Assuming the radius to be in centimeters: \n");
+    printf("The perimeter of the circle is: %0.2fcm\t %0.2fmm\t%0.2fm\n", 2 * PI * radius, 2 * PI * radius * 10, 2 * PI * radius * 0.01);
+    printf("The area of the circle is: %0.2fcm\t%0.2fmm\t%0.2f", PI * radius * radius, 10 *PI * radius * radius, 0.01*PI * radius * radius);
+    return 0;
+}
