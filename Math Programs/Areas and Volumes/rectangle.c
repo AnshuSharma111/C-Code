e22e35657249f7f524c8eee9a9b927ee951e522a
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2023-11-26 11:26:34 +0530

    add/rectangle.c

---
+++ Math Programs/Areas and Volumes/rectangle.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <math.h>
+
+// Program to find area and perimeter of rectangle or square
+
+int main(){
+    float l, b;
+    printf("Enter length and breadth of the rectangle: ");
+    scanf("%f %f", &l, &b);
+
+    printf("The perimeter of the rectangle is: %0.2f\n", 2*(l+b));
+    printf("The arae of the rectangle is: %0.2f\n", l*b);
+    printf("The diagonal of the rectangle is: %0.2f", sqrt(pow(l,2) + pow(b,2)));
+    return 0;
+}
