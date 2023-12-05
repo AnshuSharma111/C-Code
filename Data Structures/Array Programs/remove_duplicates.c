af1e659bc5095a03beebd07c3a42f823c93b4eb8
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2023-12-05 10:03:38 +0530

    fix/remove_duplicates.c

--- Data Structures/Array Programs/remove_duplicates.c
+++ Data Structures/Array Programs/remove_duplicates.c
@@ -3,20 +3,29 @@
 
 // Program to remove all duplicates from an array
 
-void remove_duplicates(int *arr, int* size){
-    int dec = 0;
-    for(int i =0; i<*size; i++){
-        for(int j = i+1; j<*size; j++){
-            if (arr[i] == arr[j]){
-                for(int k = j; k<*size;  k++){
-                        arr[k-1] = arr[k];
-                }
+int remove_duplicates(int *arr, int* size){
+    int unique[*size];
+    int u_count = 0;
+    
+    int i=0, j=0, in_uniq = 0;
+    
+    for(i=0; i<*size; i++){
+        in_uniq = 0;
+        for(j=0; j<*size; j++){
+            if (unique[j] == *(arr+i)){
+                in_uniq = 1;
+                break;
             }
         }
+        
+        if (in_uniq == 0){
+            unique[u_count] = *(arr+i);
+            u_count++;
+        }
     }
-    for (int i = 0; i<*size; i++){
-        printf("%d\t", arr[i]);
-    }
+
+    for(i = 0; i<u_count; i++)
+    printf("%d ", unique[i]);
 }
 
 int main(){
@@ -24,12 +33,15 @@ int main(){
     printf("Enter size of array: ");
     scanf("%d", &size);
     int *arr = (int*) malloc(sizeof(int) * size);
+    int *uniq = (int*) malloc(sizeof(int) * size);
 
+    printf("Enter array: ");
     for(int i =0; i<size; i++){
         scanf("%d", &arr[i]);
     }
 
     printf("\nAfter removing duplicates the array is: \n");
     remove_duplicates(arr, &size);
+
     return 0;
 }
