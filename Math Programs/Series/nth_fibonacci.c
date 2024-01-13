4ca852f2412f582b49f77b13e4145f7eabbe19e1
Keyboard-1 <142900182+Keyboard-1@users.noreply.github.com>
2024-01-13 13:57:17 +0530

    modify/nth-fibonacci.c

--- Math Programs/Series/nth_fibonacci.c
+++ Math Programs/Series/nth_fibonacci.c
@@ -1,11 +1,26 @@
 #include <stdio.h>
+#include <stdlib.h>
 
 // To calculate the n-th number of the fibonacci series
+
+int fib(int n){
+    if (n <= 1){ return 1;}
+    return (fib(n-1) + fib(n-2));
+}
 
 int main(){
     int n, a=0, b=1, c;
     printf("Enter the position of the number you want to get: ");
     scanf("%d", &n);
+
+    if (n == 1){
+        printf("The n-th number of the fibonacci series is: %d", 0);
+        exit(0);
+    }
+    if (n == 2){
+        printf("The n-th number of the fibonacci series is: %d", 1);
+        exit(0);
+    }
 
     for(int i = 2; i<n; i++){
         c = a+b;
@@ -13,6 +28,6 @@ int main(){
         b = c;
     }
 
-    printf("The n-th number of the fibonacci series is: %d" , c);
+    printf("The n-th number of the fibonacci series is: %d\n" , c);
     return 0;
 }
